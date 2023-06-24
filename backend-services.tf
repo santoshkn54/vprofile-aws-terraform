f81a27b5e89a34173a8b3af427e3b5847ff85762
santoskn54 <santhoshkn54@gmail.com>
2023-06-24 20:48:39 +0530

    memcahced

--- backend-services.tf
+++ backend-services.tf
@@ -33,7 +33,7 @@ resource "aws_elasticache_cluster" "vprofile-cache" {
   engine = "memcached"
   node_type = "cache.t2.micro"
   num_cache_nodes = 1
-  parameter_group_name = "default.memcached1.5"
+  parameter_group_name = "default.memcached1.6"
   port = 11211
   security_group_ids =[aws_security_group.vprofile-backend-sg.id]
   subnet_group_name = aws_elasticache_subnet_group.vprofile-ecache-subgroup.name
