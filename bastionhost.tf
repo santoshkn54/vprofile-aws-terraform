55b71d1999adf8dfd019c333eacf50808cd33398
santoskn54 <santhoshkn54@gmail.com>
2023-06-25 09:18:30 +0530

    updated

--- bastionhost.tf
+++ bastionhost.tf
@@ -5,6 +5,7 @@ resource "aws_instance" "vprofile-bastion" {
   subnet_id = module.vpc.public_subnets[0]
   count = var.instance_count
   vpc_security_group_ids =[aws_security_group.vprofile-bastion-sg.id]
+  associate_public_ip_address = true
 
   tags = {
     Name = "vprofile-bastion"
